2f11db65df15999722c0ea7a6c5f5e0a74e8cdbc
Manijyothi123 <155509177+Manijyothi123@users.noreply.github.com>
2024-01-09 10:06:44 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,26 @@
+
+const express = require('express')
+const app = express();
+const mongoose =require('mongoose')
+const cors = require('cors')
+
+app.get('/',(req,res)=>{res.send("hello from server..")})
+
+app.use(cors())
+app.use(express.json())
+
+const Db = async()=>{
+    try {
+        await mongoose.connect('mongodb+srv://Manijyothi:mani2003@cluster0.rk37fql.mongodb.net/?retryWrites=true&w=majority').then(
+            ()=>{
+                console.log("Connected to DataBase...")
+            }
+        )
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+Db();
+
+app.listen(5643,()=>{console.log("Server is running on port 5643...")})
